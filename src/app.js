944f9e3fb3a0c2de3ad0476101316dca0d964d46
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-15 12:55:18 +0530

    Exploring route handling in Node.js

--- src/app.js
+++ src/app.js
@@ -2,14 +2,36 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  console.log("Coming inside the first request URL");
-  res.send("Hello hello");
+app.use("/user", (req, res) => {
+  res.send({ message: "Universal user message" });
 });
 
-app.use("/test", (req, res) => {
-  console.log("Coming inside the first request URL");
-  res.send("Hello from the server");
+app.get("/user", (req, res) => {
+  res.send({ name: "Prerak Parekh", city: "Mumbai" });
+});
+
+app.post("/user", (req, res) => {
+  res.send({ message: "Successfully post called to user API" });
+});
+
+app.delete("/user", (req, res) => {
+  res.send({ message: "User deleted successfully" });
+});
+
+app.use("/group", (req, res) => {
+  res.send({ message: "Universal group message" });
+});
+
+app.get("/group", (req, res) => {
+  res.send({ name: "Prerak Parekh group", city: "Mumbai group" });
+});
+
+app.post("/group", (req, res) => {
+  res.send({ message: "Successfully post called to group API" });
+});
+
+app.delete("/group", (req, res) => {
+  res.send({ message: "Group deleted successfully" });
 });
 
 app.listen(3000, () => {
