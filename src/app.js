40e63d9b5a92fd052efa7a8b177d84796ca69457
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-15 22:18:39 +0530

    Added multiple route handlers and different ways to add it

--- src/app.js
+++ src/app.js
@@ -2,45 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello/:userId/:officeId", (req, res) => {
-  console.log(req.query);
-
-  console.log(req.params);
-
-  res.send({ message: "Hello from hello URL 123" });
-});
-
-app.use("/user", (req, res) => {
-  res.send({ message: "Universal user message" });
-});
-
-app.get("/user", (req, res) => {
-  res.send({ name: "Prerak Parekh", city: "Mumbai" });
-});
-
-app.post("/user", (req, res) => {
-  res.send({ message: "Successfully post called to user API" });
-});
-
-app.delete("/user", (req, res) => {
-  res.send({ message: "User deleted successfully" });
-});
-
-app.use("/group", (req, res) => {
-  res.send({ message: "Universal group message" });
-});
-
-app.get("/group", (req, res) => {
-  res.send({ name: "Prerak Parekh group", city: "Mumbai group" });
-});
-
-app.post("/group", (req, res) => {
-  res.send({ message: "Successfully post called to group API" });
-});
-
-app.delete("/group", (req, res) => {
-  res.send({ message: "Group deleted successfully" });
-});
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+      console.log("Route handler 1");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Route handler 2");
+      next();
+    },
+  ],
+  (req, res) => {
+    console.log("Route handler 3");
+    res.send({ message: "Hello" });
+  }
+);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000....");
