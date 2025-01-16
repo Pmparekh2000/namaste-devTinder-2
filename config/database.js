055e2d57958e6305cead446613d858c8ba6984a9
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-16 21:47:44 +0530

    Created User model instance and added data to users collection

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToCluster = async () => {
   await mongoose.connect(
-    "mongodb+srv://admin:admin@prerakcluster.tq5ia.mongodb.net/?retryWrites=true&w=majority&appName=Prerakcluster/devTinder3"
+    "mongodb+srv://admin:admin@prerakcluster.tq5ia.mongodb.net/devTinder3"
   );
 };
 
--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const { connectToCluster } = require("../config/database");
+const { User } = require("./models/user");
 
 const app = express();
 const PORT = 3000;
+
+app.post("/signup", async (req, res) => {
+  const dummyUser = {
+    firstName: "Hiten",
+    lastName: "Parekh",
+    emailId: "hpp.tech@gmail.com",
+    password: "987654321",
+    age: 52,
+    gender: "female",
+  };
+
+  try {
+    // Creating a new instance of the User model.
+    const user = new User(dummyUser);
+    const response = await user.save();
+
+    res.status(200).send({
+      message: `User ${response.firstName} with email ${response.emailId} saved successfully`,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: `Something went wrong while creating user ${dummyUser.firstName}`,
+      error: error.message,
+    });
+  }
+});
 
 connectToCluster()
   .then(() => {
