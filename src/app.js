dc3e7507ccc934f028c422fe724aadf53de0d86a
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-16 05:53:15 +0530

    Understood the concept of middleware

--- src/app.js
+++ src/app.js
@@ -2,23 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  [
-    (req, res, next) => {
-      console.log("Route handler 1");
-      next();
-    },
-    (req, res, next) => {
-      console.log("Route handler 2");
-      next();
-    },
-  ],
-  (req, res) => {
-    console.log("Route handler 3");
-    res.send({ message: "Hello" });
-  }
-);
+app.use("/user", (req, res, next) => {
+  console.log("Route handler 2");
+  next();
+});
+
+app.use("/user", (req, res, next) => {
+  console.log("Route handler 1");
+  next();
+});
+
+app.use("/user123", (req, res, next) => {
+  console.log("Route handler 123");
+  res.send({ message: "Hello 123" });
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000....");
