839626ef0b4e7b606548e9d9b2a61ea622f8488c
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-22 14:45:04 +0530

    Added /feed API

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const { ConnectionRequest } = require("../src/models/connectionRequest");
 const { INTERESTED, ACCEPTED } = require("../src/utils/constants");
+const { User } = require("../src/models/user");
 const userRouter = express.Router();
 
 // Get all the pending connection requests for the logged in user
@@ -62,6 +63,44 @@ userRouter.get("/connections", userAuth, async (req, res) => {
     res.status(200).send({
       message: "All of your connections are : ",
       connections: data,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message:
+        "Something went wrong while fetching all of your existing connections",
+      error: error.message,
+    });
+  }
+});
+
+// Get the feed of the current user
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // Find all connection request either I have sent or received
+    const loggedInUserConnections = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    loggedInUserConnections.map((loggedInUserConnection) => {
+      hideUsersFromFeed.add(loggedInUserConnection.toUserId.toString());
+      hideUsersFromFeed.add(loggedInUserConnection.fromUserId.toString());
+    });
+
+    // Below second $ne condition is required since for the first time login user
+    // there would be no connection and hence the hideUsersFromFeed set will be empty
+    const userFeedData = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    });
+    res.status(200).send({
+      message: "All available users in our db are",
+      count: userFeedData.length,
+      users: userFeedData,
     });
   } catch (error) {
     res.status(400).send({
